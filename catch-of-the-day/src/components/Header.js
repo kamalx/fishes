fa75cd15c8dc6a93299e1d1d1e86d8729bcdddef
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-27 23:37:31 +0530

    Add a Header component and import it in App

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <p>Header</p>
+        )
+    }
+}
+
+export default Header;
