01850f1b8c522836815ec18a453cb1756f0fcb84
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-27 23:27:55 +0530

    Create an App component scaffold

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    { /* scaffold for the app */ }
+                </div>
+            </div>
+        )
+    }
+}
