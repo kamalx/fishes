6156526407f14fc67eb50cd67ed93ac8d10ba715
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-28 00:38:20 +0530

    Convert into stateless functional component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class Header extends React.Component {
-    render() {
-        console.log(this);
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">of</span>
-                        <span className="the">the</span>
-                    </span>
-                    Day
-                </h1>
-            {/*
-            `this` is the component.
-            `props` is the object.
-            `tagline` is the variable we wish to access.
-            */}
-                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-            </header>
-        )
-    }
+// Converted this into a stateless functional component
+// REF: https://facebook.github.io/react/docs/components-and-props.html
+// Instruction video 8
+const Header = (props) => {
+    return (
+        <header className="top">
+            <h1>
+                Catch
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day
+            </h1>
+        {/*
+        `this` is the component.
+        `props` is the object.
+        `tagline` is the variable we wish to access.
+        */}
+            <h3 className="tagline"><span>{props.tagline}</span></h3>
+        </header>
+    )
 }
 
 export default Header;
