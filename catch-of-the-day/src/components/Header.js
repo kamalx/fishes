5e08fa04914e952a2a4b454a06bbc999625ba617
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-28 00:25:17 +0530

    Add some decoration using predefined styles
    These were as instructed in the tutorial. The stylesheet has been
    supplied and it's pretty finished styling that gets applied as
    soon as a relevant element structure with relevant tags and
    classnames is added.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -3,15 +3,23 @@ import {render} from 'react-dom';
 
 class Header extends React.Component {
     render() {
+        console.log(this);
         return (
             <header className="top">
-                <h1>Catch of the Day</h1>
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                    Day
+                </h1>
             {/*
             `this` is the component.
             `props` is the object.
             `tagline` is the variable we wish to access.
             */}
-                <h3 className="tagline">{this.props.tagline}</h3>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
             </header>
         )
     }
