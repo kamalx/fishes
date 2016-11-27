89aa89aaa0a61efab5d8279f848488fe8d92d495
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-27 23:02:33 +0530

    Create a StorePicker component with createElement to render html

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+    render() {
+        // return some jsx
+        return React.createElement('p', { className: 'testing'}, 'I love you');
+    }
+}
+
+export default StorePicker;
