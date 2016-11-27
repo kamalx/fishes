b558596ec8eb70fd3789bf1b6b1692b7de2404da
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-27 23:13:49 +0530

    JSX comments are annoying
    These need to be wrapped inside the parent container element too.
    You can't have a JSX comment adjacent to the parent HTML container
    element inside the return (...) statement.
    
    However, normal JS comments work outside of the return statement.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,8 +4,16 @@ import { render } from 'react-dom';
 class StorePicker extends React.Component {
     render() {
         // return some jsx
+        // Let's enter some jsx comments
         return (
             <form className="store-selector">
+            {/*
+                This weird abomination is a jsx comment!
+
+                And the fun thing is that this comment cannot be adjacent to
+                the top level container element either. It should be wrapped
+                inside it too!
+            */}
                 <h2>Please Enter A Store...</h2>
                 <input type="text" required placeholder='Store Name' />
                 <button type="submit">Visit Store</button>
