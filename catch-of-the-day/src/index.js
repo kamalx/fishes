6f29f986d6c7972acf82972b296e37dcda4d39bd
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-28 01:18:40 +0530

    Include some routing and a 404 page as NotFound component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector("#main"));
+// We're adding the router component right here instead of in a new file
+// for now.
+// This is a stateless functional component like the one we saw in just previous
+// commit.
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match exactly pattern="/store/:storeId" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+render(<Root/>, document.querySelector("#main"));
