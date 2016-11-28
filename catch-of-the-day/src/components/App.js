ba727767559171571b49ae375bd68a7352b71b24
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-28 14:35:36 +0530

    Add an initial state to the App constructor

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,15 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        // getInitialState
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
     render() {
         return(
             <div className="catch-of-the-day">
