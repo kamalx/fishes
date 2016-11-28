32e86119e153a4337d8007283b99afe8923bb93a
Kamaleshwar Morjal <kamal@morjal.com>
2016-11-29 01:14:06 +0530

    Iterate over the list of fishes and add Fish component for each
    We use Object.keys(JSO) to iterate over the this.state.fishes
    object. This returns an array of keys which we can then use to
    iterate over the object and use the contained data to render
    our view whatever way we like.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
@@ -42,6 +43,13 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Sea Food Market" />
+                    <ul className="list-of-fish">
+                        {
+                            Object
+                                .keys(this.state.fishes)
+                                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                        }
+                    </ul>
                 </div>
                 <Order/>
                 <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
