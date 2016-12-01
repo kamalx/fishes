ac8bf8eaa45b2110c37be3a7c1b1f1d5f3204584
Kamaleshwar Morjal <kamal@morjal.com>
2016-12-01 13:43:07 +0530

    Add a sample config file for firebase API

---
+++ catch-of-the-day/src/sample_config.js
@@ -0,0 +1,11 @@
+// This is a sample configuration file for your app's
+// firebase database connection.
+// Replace appropriate values for the given keys that
+// you'll find from your firebase account into the JS
+// object below and rename this file to `config.js`
+// and everything else should work out of the box.
+export const config = {
+    "apiKey": {YOUR API KEY FROM FIREBASE ACCOUNT},
+    "authDomain": {YOUR AUTH DOMAIN FROM FIREBASE ACCOUNT},
+    "databaseURL": {YOUR DATABASE URL FROM FIREBASE ACCOUNT}
+};
